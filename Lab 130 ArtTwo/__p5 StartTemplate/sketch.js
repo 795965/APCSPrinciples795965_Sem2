59b32dd0584060ec69dcef4ae642ea5324a93c46
795965 <42558488+795965@users.noreply.github.com>
2020-01-30 08:21:16 -0800

    finished

---
+++ Lab 130 ArtTwo/__p5 StartTemplate/sketch.js
@@ -0,0 +1,32 @@
+//  Ziggy Sheynin
+// 	Lab 130- Art Two
+//  This is a comment
+//  The setup function function is called once when your program begins
+
+var boids = []; //initializes boids array
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+
+  loadBoids(60); //calls function loadBoids
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+runBoids();
+}
+
+function loadBoids(n){
+  for (var i =0; i< n; i++){
+    boids[i] = new Boid(random(width), random(height), random(-1,1), random(-1,1), 40); //initializes and declares bid objects
+    }
+  }
+
+function runBoids(){
+  for (var i =0; i<boids.length; i++){
+    boids[i].run();
+    }
+
+}//+++++++++++++++++++++++++++end sketch
